785db273a5e659197b87a66303dce6e555c0063c
Umesh Chaudhari <chaudhariumesh5534@gmail.com>
2024-03-20 09:25:45 +0530

    Second time Committed.

--- app/src/main/java/com/umesh_app/weather/presentation/WeatherCard.kt
+++ app/src/main/java/com/umesh_app/weather/presentation/WeatherCard.kt
@@ -3,7 +3,6 @@
 package com.umesh_app.weather.presentation
 
 
-import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
